e3f09df604c245d904c480f5d884625d53835351
josewalter <josewalter408@gmail.com>
2025-06-15 12:00:51 -0300

    Primeiro commit, criando e configurando o projeto.

---
+++ src/main/java/com/apideconsultadecreditos/api_de_consulta_/de_creditos/ApiDeConsultaDeCreditosApplication.java
@@ -0,0 +1,13 @@
+package com.apideconsultadecreditos.api_de_consulta_.de_creditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeConsultaDeCreditosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeConsultaDeCreditosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apideconsultadecreditos/api_de_consulta_/de_creditos/ApiDeConsultaDeCreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apideconsultadecreditos.api_de_consulta_.de_creditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeConsultaDeCreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
